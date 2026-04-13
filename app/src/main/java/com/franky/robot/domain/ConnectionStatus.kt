211794a9a06f5354f459267f27576b51a81265ab
martinentraigas07-design <martinentraigas07@gmail.com>
2026-04-13 13:21:26 -0300

    Delete app directory

--- app/src/main/java/com/franky/robot/domain/ConnectionStatus.kt
+++
@@ -1,8 +0,0 @@
-package com.franky.robot.domain
-
-enum class ConnectionStatus {
-    DISCONNECTED,
-    SCANNING,
-    CONNECTING,
-    CONNECTED
-}
--- app/src/main/java/com/franky/robot/domain/SensorData.kt
+++
@@ -1,7 +0,0 @@
-package com.franky.robot.domain
-
-data class SensorData(
-    val adc0: Int = 0,
-    val temp: Float = 0f,
-    val hum: Float = 0f
-)
